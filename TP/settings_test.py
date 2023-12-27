152bcd78869958d6c24e85b427cda79f1c2658af
assala <reb.assala@gmail.com>
2023-12-27 11:34:36 +0100

    test unitaire de Upload

---
+++ TP/settings_test.py
@@ -0,0 +1,7 @@
+# settings_test.py
+from .settings import *
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / "db_test.sqlite3",
+}
--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,17 @@
+#implementation des tests unitaires
+import os
 from django.test import TestCase
+from .serializers import PDFTxtExtractionSerializer
 
-# Create your tests here.
+class PDFTxtExtractionSerializerTest(TestCase):
+    def setUp(self):
+        self.serializer = PDFTxtExtractionSerializer()
+        # Spécifiez le chemin complet du fichier PDF que vous souhaitez utiliser pour le test
+        self.pdf_path = os.path.join(os.path.dirname(__file__), 'C:/Users/ThinkPad X280/Desktop/TP/articles/Article_01.pdf')
+
+    def test_extract_text_from_pdf(self):
+     # Appelez la méthode d'extraction de texte du serializer
+     extracted_text = self.serializer.extract_text_from_pdf(self.pdf_path)
+
+     # Vérifiez si le texte extrait n'est pas vide
+     self.assertTrue(extracted_text, "Aucun texte n'a été extrait du PDF.")
